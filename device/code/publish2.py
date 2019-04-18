264610fca53b1fa0b08f1ef762cef7771125f9a1
NDT <nguyenduytamgithub@gmail.com>
2019-04-18 11:29:48 +0700

    update last

--- device/code/publish2.py
+++ device/code/publish2.py
@@ -3,30 +3,31 @@ import sys
 import paho.mqtt.client as mqtt
 import json
 import base64
-THINGSBOARD_HOST = '115.79.27.129'
+import ssl
+THINGSBOARD_HOST = '209.97.173.222'
 #THINGSBOARD_HOST = '192.168.1.19'
 client = mqtt.Client()
 # Connect to ThingsBoard using default MQTT port and 60 seconds keepalive interval
-client.connect(THINGSBOARD_HOST, 1883, 60)
-
+client.tls_set('C:/Users/nguye/Desktop/TRANS_FILE/ca.crt')
+client.tls_insecure_set(False)
+client.connect(THINGSBOARD_HOST, 8883, 60)
 client.loop_start()
 
 try:
    
-        # Sending humidity and temperature data to ThingsBoard
-        #client.publish('synch', json.dumps('ok'), 1)
-	f1=open("../image/image.jpg", "rb") #3.7kiB in same folder
-	fileContent = f1.read()
+    #client.publish('synch', json.dumps('ok'), 1)
+	#f1=open("../image/image.jpg", "rb") #3.7kiB in same folder
+	#fileContent = f1.read()
 	#imageArray = bytearray(fileContent)
-	imageArray = base64.b64encode(fileContent).replace('\n','')
+	#imageArray = base64.b64encode(fileContent).replace('\n','')
 	#client.publish('synch', imageArray, 1)
-        f2=open("../image/image_name.txt", "rb") #3.7kiB in same folder
-        imageName = f2.read().replace('\n','')
+	#f2=open("../image/image_name.txt", "rb") #3.7kiB in same folder
+	#imageName = f2.read().replace('\n','')
 	send_msg = {
-        	'image_name': imageName,
-		'image_array': imageArray
+		'image_name': "duy tam",
+		'image_array': "dep trai"
 	}
-	client.publish('synch',json.dumps(send_msg), 0)	
+	client.publish('abc',json.dumps(send_msg), 0)	
 except KeyboardInterrupt:
     pass
 
--- device/code/shoot.sh
+++ device/code/shoot.sh
@@ -3,7 +3,7 @@
 echo "Killall mjpg_streamer"
 killall mjpg_streamer
 
-echo "Start fucking mjpg_streamer"
+echo "Start mjpg_streamer"
 /usr/local/bin/mjpg_streamer -i "input_uvc.so -r 1280x720 -d /dev/video0 -f 30 -q 100" -o "output_http.so -p 8080 -w /usr/local/share/mjpg-streamer/www" > /dev/null 2>&1 &
 
 echo "Start setup camera"
@@ -11,19 +11,20 @@ echo "Start setup camera"
 echo "Init..."
 sleep 10
 echo "Init...OK"
-while true; do 
+#while true; do
+	link="/root/synch_server/device"
 	datetime="$(date +%Y-%m-%d-%H_%M_%S)"
-	filename="/home/pi/NDT/dataset/class-${datetime}.jpg"
+	filename="${link}/img_storage/${datetime}.jpg"
 	echo "We will export to ${filename}"
-	echo -e "class-${datetime}" >> /home/pi/NDT/dataset/dataset.csv  > /home/pi/NDT/synch/image/image_name.txt
+	echo -e "${datetime}" >> ${link}/excel/dataset.csv  > ${link}/image/image_name.txt
 	wget http://127.0.0.1:8080/?action=snapshot -O ${filename}
-	cp ${filename}  /home/pi/NDT/synch/image/image.jpg
-	sleep 1
-	echo "Start synch image"
-	python /home/pi/NDT/synch/code/publish2.py
-	echo "Finish synch image - go to sleep"
-	sleep 10
-done
+	cp ${filename} ${link}/image/image.jpg
+	#sleep 1
+	#echo "Start synch image"
+	#python /home/pi/NDT/synch/code/publish2.py
+	#echo "Finish synch image - go to sleep"
+	#sleep 10
+#done
 
 exit 0
 
